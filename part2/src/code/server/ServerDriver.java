822b6c040627fee95452c2c08cc09e0b9008a1f2
Jason(Jaeheun) Kim <jaeheunk@buffalo.edu>
2015-04-27 17:10:09 -0400

    rmi

--- part2/src/code/server/ServerDriver.java
+++ part2/src/code/server/ServerDriver.java
@@ -1,5 +1,16 @@
 package code.server;
 
-public class ServerDriver {
+import java.io.IOException;
+import java.util.ArrayList;
 
-}
+public class ServerDriver {
+	public static void main(String[] args) throws IOException {
+		ArrayList<String> ag = new ArrayList<String>();
+		for(String s : args){
+			ag.add(s);
+		}
+		String host = ag.get(0);
+		int port = Integer.parseInt(ag.get(1));
+		}
+	}
+

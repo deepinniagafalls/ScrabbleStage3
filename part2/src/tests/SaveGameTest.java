65d6c1af2f640e448d42f1ad159441ba4524d235
Jason(Jaeheun) Kim <jaeheunk@buffalo.edu>
2015-05-07 11:01:00 -0400

    rmi stuff and bugs fixed

--- part2/src/tests/SaveGameTest.java
+++ part2/src/tests/SaveGameTest.java
@@ -15,11 +15,11 @@ public class SaveGameTest {
 	
 	@Test
 	public void saveStringTest() {
-		BufferedWriter bw = new BufferedWriter(bw);
-		for(int i = 0; i < _scrabble.getInv().getSize(); i = i + 1 ){
-			bw.write("[" + _scrabble.getInv().getTile(i).getChar() + "]");
-		}
-		assertTrue("The string that is saved is" +);
+		//BufferedWriter bw = new BufferedWriter(bw);
+		//for(int i = 0; i < _scrabble.getInv().getSize(); i = i + 1 ){
+		//	bw.write("[" + _scrabble.getInv().getTile(i).getChar() + "]");
+		//}
+		//assertTrue("The string that is saved is" +);
 	}
 
 }

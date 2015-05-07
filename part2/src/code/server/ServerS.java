037b1e14c8c6ebaace3fd5c3d2bc7b0e92d8089e
Jason(Jaeheun) Kim <jaeheunk@buffalo.edu>
2015-05-07 11:45:21 -0400

    stuff rmi yeah

--- part2/src/code/server/ServerS.java
+++ part2/src/code/server/ServerS.java
@@ -3,6 +3,7 @@ package code.server;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+import alphonce.i.IClient;
 import code.base.Board_024;
 import code.base.Player_024_047;
 import code.client.Game_047;
@@ -18,9 +19,12 @@ public class ServerS implements ServerI, Runnable {
 	private static int _currentTurn = 0;
 	private static Game_047 _currentGame;
 	private ArrayList<String> _names = new ArrayList<>();
+	private ArrayList<ClientI> _observers;
 	
 	public ServerS(){ 
 		System.out.println("...server running...");
+		_observers = new ArrayList<ClientI>();
+
 		
 	}
 
@@ -32,7 +36,8 @@ public class ServerS implements ServerI, Runnable {
 
 	@Override
 	public void addClientI(ClientI c) throws RemoteException {
-		// TODO Auto-generated method stub
+		System.out.println("[SERVER] Adding client");
+		_observers.add(c);
 		
 	}
 

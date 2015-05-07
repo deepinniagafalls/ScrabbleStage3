c259c28c833d10de5899f5ad33c220d57205d7aa
Jason(Jaeheun) Kim <jaeheunk@buffalo.edu>
2015-05-07 18:37:47 -0400

    RMI

--- part2/src/code/i/ServerI.java
+++ part2/src/code/i/ServerI.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public interface ServerI extends Remote {
 	//this interface specifies all the methods that are to be called by a client to a server.
 	// Client -> this -> Methods in Server
+	public static final String SERVER_REGISTRY_KEY = "Player";
 	public void addClientI(ClientI c) throws RemoteException;
 	public void start(String data) throws RemoteException;
 	public void sendName(String name) throws RemoteException;
